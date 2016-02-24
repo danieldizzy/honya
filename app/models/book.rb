bf6a9436b32cdad72b33725c029cde7535d0b6d7
Daniel Dizzy <dizzylol4@gmail.com>
2016-02-24 23:42:11 +0900

    Added a sale model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,13 @@
 class Book < ActiveRecord::Base
 extend FriendlyId
-friendly_id :name, use: :slugged 
+friendly_id :name, use: :slugged
 
   belongs_to :user
+  has_many :sales
   has_attached_file :image
   has_attached_file :resource
+
+  validates_numericality_of :price, greather_then: 1, message: "Must be at least 1 Yen"
 
   validates_attachment_content_type :image, content_type: /^image\/(png|gif|jpeg)/, message: "Only images allowed"
 
