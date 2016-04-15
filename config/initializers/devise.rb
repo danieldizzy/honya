1ae658a99ead1bd35f4270fcb110fbd4257b97c9
Daniel Dizzy <dizzylol4@gmail.com>
2016-04-16 03:56:12 +0900

    Dumped to heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
-  # config.secret_key = '5aabc7d9ed5a27c66ec277ac2b718422a4b6986ae767085dad648e9301c5e5e91ec29ccc0e7c72a8900edb90fe8155ccf9e3af4f4f18d560c5b5c7162a8d4890'
+  config.secret_key = '5aabc7d9ed5a27c66ec277ac2b718422a4b6986ae767085dad648e9301c5e5e91ec29ccc0e7c72a8900edb90fe8155ccf9e3af4f4f18d560c5b5c7162a8d4890'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
